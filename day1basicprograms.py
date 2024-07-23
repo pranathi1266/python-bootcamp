6cf5609a9516e36500b1e916c849013146dd127e
pranathi1266 <21d21a1266gpranathi@gmail.com>
2024-07-23 14:56:34 +0530

    Add files via upload

---
+++ day1basicprograms.py
@@ -0,0 +1,145 @@
+##############data types############
+'''1.int
+2.string
+3.sets
+4.float
+5.dict
+6.tuple
+7.boolean'''
+#to print something
+#print("sridevi")
+
+#var1="python"
+#print("good morning",var1)
+#var=input()
+#print("good mrg",var)
+##hlo
+#good mrg hlo
+
+#Name=input()
+#print("good mrg",name)
+##hlo
+#good mrg hlo
+
+#Name=input("enter your name :")
+#print("good mrg",Name)
+##enter your name : hlo
+#good mrg hlo
+
+#name=input()
+'''age=(intput())
+print(f"hello{name}you are {age} years old")'''
+##pranathi
+#20
+#hello pranathi you are 20 years old
+
+#name=input()
+'''avg=0
+sum=0
+for i in range(0,5):
+    marks=int(input())
+    sum+=marks
+avg=sum/5
+print(f"your name is {name} and your sum is {sum} and your avg is {avg}")'''
+##pranathi
+#3
+#4
+#5
+#6
+#7
+#your name is pranathi and your sum is 25 and your avg is 5.0 
+
+#name=input()
+'''print(name,end=".")
+print(name)'''
+##hlo
+#hlo.hlo
+
+
+#name=input()
+'''print(name,end="\n")
+print(name)'''
+##hlo
+#hlo
+#hlo
+
+#name=input()
+'''print(name,end=".")'''
+##hello
+#hello.
+
+########conditional statements########
+#if
+#elif
+#else
+
+'''age=int(input())
+if (age<=20):
+    print("yes")
+else:
+    print("no")'''
+##8
+#yes
+
+'''name=input()
+age=int(input())
+if (age>=18):
+    print(f"your name is {name} and your are  eligible for voting")
+
+else:
+    print(f"your name is {name} and your are not eligible for voting")'''
+##pranathi
+#40
+#your name is pranathi and your are  eligible for voting
+'''a=int(input())
+b=int(input())
+print(f"the sum of the num{a+b} and the subtraction is {a-b}")or
+print(a-b)
+print(a+b)'''
+#$a//b -remainder
+#$a**b -power and using math fun -power of like 2 power of 2
+
+
+######logical operators######
+###$logical and
+'''age=int(input())
+if(age<=18 and age<24):
+    print("only two wheeler")
+elif(age<=24 and age>45):
+    print("2 wheeler and 4 wheeler")
+else:
+    print("4 wheeler and 6 wheeler")'''
+##32
+#2 wheeler and 4 wheeler
+
+
+#Qns-10 apples per1-15
+ #   24 bananas per1-4
+#    8 oranges per1-5
+#    total mom rs given 700 
+'''a=int(input())
+b=int(input())
+c=int(input())
+n=700
+total=a*15 + b*4 + c*5
+if(total<=n):
+    print("not cheated")
+else:
+    print("cheated")'''
+##10
+#24
+#8
+#not cheated
+
+'''a=int(input())
+if(a>0 and a%2==0):
+    print("positive and even")
+elif(a<0 and a%2!=0):
+    print("positive and odd")
+elif(a<0 and a%2==0):
+    print("neg and even")
+elif(a>0 and a%2!=0):
+    print("neg and odd")'''
+##8
+#positive and even
+
